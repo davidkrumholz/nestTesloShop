886f95d3284dc180d11765993d3ef01a76379be4
David Krumholz <david.krumholz@bettercloud.com>
2026-01-19 13:25:17 -0600

    Add guards for user role and auth decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Post, Body, UseGuards, Headers } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Headers, SetMetadata } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
-import { GetUser, RawHeaders } from './decorators/';
+import { Auth, GetUser, RawHeaders, RoleProtected } from './decorators/';
 import { User } from './entities/user.entity';
 import type { IncomingHttpHeaders } from 'http';
+import { UserRoleGuard } from './guards/user-role/user-role.guard';
+import { ValidRoles } from './interfaces';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -36,4 +38,28 @@ export class AuthController {
       headers,
     }
   }
+
+  @Get('private2')
+  @RoleProtected(ValidRoles.admin)
+  @UseGuards(AuthGuard(), UserRoleGuard)
+  testingPrivateRoute2(
+    @GetUser() user: User,
+  ) {
+    return {
+      ok: true,
+      user,
+    }
+  }
+
+  @Get('private3')
+  @Auth(ValidRoles.superUser, ValidRoles.admin)
+  testingPrivateRoute3(
+    @GetUser('email') user: User,
+  ) {
+    return {
+      ok: true,
+      user,
+    }
+  }
+  
 }
